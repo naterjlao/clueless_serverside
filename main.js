01fe246b37a22860beef35ce49e45ce72cbe6c37
Nate Lao <lao.nathan@yahoo.com>
2020-04-12 23:52:13 -0400

    canonical serverside router

--- main.js
+++ main.js
@@ -12,7 +12,12 @@
 /******************************************************************************
 * GLOBAL CONSTANTS
 ******************************************************************************/
-const LISTENER_PY = "/opt/clueless/src/serverside/listener.py"
+const BACKEND_WRAPPER = "/opt/clueless/src/serverside/listener.py";
+
+/******************************************************************************
+* IMPORTS
+******************************************************************************/
+const crypto = require('crypto);
 
 /******************************************************************************
 * SETUP THE NETWORK CONFIGURATION
@@ -21,7 +26,7 @@ log('SETTING UP NETWORK CONFIGURATION...');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var clientSocket = require('socket.io')(http);
+var mainSocket = require('socket.io')(http); // This pushes to everyone!
 var spawn = require("child_process").spawn;
 log('NETWORK CONFIGURATION ESTABLISHED...');
 
@@ -29,8 +34,18 @@ log('NETWORK CONFIGURATION ESTABLISHED...');
 * SPAWN PYTHON LISTENER
 ******************************************************************************/
 log('SPAWNING PYTHON LISTENER THREAD...');
-var backend = spawn("python3",[LISTENER_PY]); // TODO send output of spawn to log
+var backend = spawn("python3",[BACKEND_WRAPPER]); // TODO send output of spawn to log
 log('PYTHON LISTENER THREAD SPAWNED');
+
+/******************************************************************************
+* INSTANCE VARIABLES
+******************************************************************************/
+
+/*
+	Stores the socket object for each player.
+	Every element has an associated playerID
+*/
+players = [];
 
 /******************************************************************************
 * AUXILIARY FUNCTIONS
@@ -38,19 +53,112 @@ log('PYTHON LISTENER THREAD SPAWNED');
 /* Log wrapper */
 function log(message) {
 	// TODO send to a log file
-	console.log(message)
+	console.log(message);
 }
 
+/* Hashes the player to generate an ID string for each Player */
+function createPlayerID(player) {
+	// TODO - this might not work
+	console.log(String(player)); // TESTING
+	
+	return crypto.createHash('sha1').update(player).digest('hex');
+}
+
+/* Adds a Player to the list of Players */
+function addPlayer(player) {
+	hash = createPlayerID(player);
+	player.playerID = hash;
+	console.log("ServerSide: adding player ".concat(hash));
+	player.join(playerID);   // the player is joined in a "room" named after the playerID
+	players.push(player);    // add the player to player list
+}
+
+/* Returns the Player that is associated with the PlayerID */
+function getPlayer(playerID) {
+	target = null;
+	for each (player in players) {
+		if (player.playerID == playerID) {
+			target = player;
+		}
+	}
+	return target;
+}
+
+/* Removes the Player from the Server */
+function removePlayer(player) {
+	player.leave(player.playerID);
+}
+
+/* Shoots a signal to the backend listener */
+function sendToBackend(playerID,eventName,sendData) {
+	if (sendData == null) {
+		signal = {
+			'playerID' : playerID,
+			'eventName': eventName
+		};
+	}
+	else {
+		signal = {
+			'playerID' : playerID,
+			'eventName': eventName,
+			'payload'  : sendData
+		};
+	}
+	signal = JSON.stringify(signal);	// absolutely necessary
+	signal.concat('\n');				// also absolutely necessary
+	backend.stdin.write(signal);		// Spit out to stdin!
+}
 
 /******************************************************************************
 * MAIN FUNCTIONS
 ******************************************************************************/
-
 /* Call the http listener */
 http.listen(3000, '0.0.0.0', () => { // TODO need configuration call
     log('Listening at 0.0.0.0:3000...');
 });
 
+/******************************************************************************
+* Backend -> Server
+******************************************************************************/
+
+/* Listens for whatever the Backend spits out 
+*  Note that this is continuous, any info flushed from the Backend stdout is
+*  processed through here.
+*/
+backend.stdout.on('data', (data) => {
+    signal = JSON.parse(data);
+	// Spit to an individual player
+	if (signal.playerID != "all") {
+		player = getPlayer(signal.playerID);
+		player.emit(signal.eventName,signal.payload);
+	}
+	// Spit to all of the players!
+	else {
+		mainSocket.emit(signal.eventName,signal.payload);
+	}
+});
+
+/******************************************************************************
+* Client -> Server
+******************************************************************************/
+
+// TODO this might be temporary
+mainSocket.on('connection', player => {
+	
+	// Add the player to the players list and have them join
+	addPlayer(player);
+	
+	// Create event handlers for the player
+	player.on('enteredGame', () => {
+		sendToBackend(player.playerID,'enteredGame',null);
+	});
+	player.on('move', (direction) => {
+		sendToBackend(player.playerID,'move',direction);
+	});
+	player.on('pass_turn', () => {
+		sendToBackend(player.playerID,'pass_turn',null);
+	});
+};
 /*
 
 // When a signal is emmitted from the Client,
